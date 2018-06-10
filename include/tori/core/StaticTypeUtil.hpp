9a2166011dd5792f5d09581cd04d46e5661d30af
mocabe <hisa.mocabe@gmail.com>
2018-06-10 20:55:27 +0900

    :wrench: escape names

--- include/tori/core/StaticTypeUtil.hpp
+++ include/tori/core/StaticTypeUtil.hpp
@@ -134,59 +134,59 @@ namespace TORI_NS::detail {
   // Subst
   // ------------------------------------------
   template <class TyArrow, class Ty>
-  struct subst {};
+  struct subst_ {};
   template <class TyT1, class TyT2, class T1, class T2>
-  struct subst<tyarrow<TyT1, TyT2>, arrow<T1, T2>> {
+  struct subst_<tyarrow<TyT1, TyT2>, arrow<T1, T2>> {
     using type = arrow<
-      typename subst<tyarrow<TyT1, TyT2>, T1>::type,
-      typename subst<tyarrow<TyT1, TyT2>, T2>::type>;
+      typename subst_<tyarrow<TyT1, TyT2>, T1>::type,
+      typename subst_<tyarrow<TyT1, TyT2>, T2>::type>;
   };
   template <class TyT1, class TyT2, class Tag>
-  struct subst<tyarrow<TyT1, TyT2>, value<Tag>> {
+  struct subst_<tyarrow<TyT1, TyT2>, value<Tag>> {
     using type = value<Tag>;
   };
   template <class TyT1, class TyT2, class Tag>
-  struct subst<tyarrow<TyT1, TyT2>, var<Tag>> {
+  struct subst_<tyarrow<TyT1, TyT2>, var<Tag>> {
     using type =
       std::conditional_t<std::is_same_v<TyT1, var<Tag>>, TyT2, var<Tag>>;
   };
   /// Apply TyArrow to Ty
   template <class TyArrow, class Ty>
-  using subst_t = typename subst<TyArrow, Ty>::type;
+  using subst_t = typename subst_<TyArrow, Ty>::type;
 
   template <class Arrows, class Ty>
-  struct subst_all {};
+  struct subst_all_ {};
   template <class Ty>
-  struct subst_all<std::tuple<>, Ty> {
+  struct subst_all_<std::tuple<>, Ty> {
     using type = Ty;
   };
   template <class TyArrow, class... Tail, class Ty>
-  struct subst_all<std::tuple<TyArrow, Tail...>, Ty> {
+  struct subst_all_<std::tuple<TyArrow, Tail...>, Ty> {
     using type =
-      typename subst_all<std::tuple<Tail...>, subst_t<TyArrow, Ty>>::type;
+      typename subst_all_<std::tuple<Tail...>, subst_t<TyArrow, Ty>>::type;
   };
   /// Apply all TyArrows to Ty
   template <class TyArrows, class Ty>
-  using subst_all_t = typename subst_all<TyArrows, Ty>::type;
+  using subst_all_t = typename subst_all_<TyArrows, Ty>::type;
 
   // ------------------------------------------
   // Subtype
   // ------------------------------------------
   template <class T1, class T2>
-  struct subtype : std::false_type {};
+  struct subtype_ : std::false_type {};
 
   template <class Tag1, class Tag2>
-  struct subtype<value<Tag1>, value<Tag2>> {
+  struct subtype_<value<Tag1>, value<Tag2>> {
     static constexpr bool value = std::is_base_of_v<Tag1, Tag2>;
   };
 
   template <class T11, class T12, class T21, class T22>
-  struct subtype<arrow<T11, T12>, arrow<T21, T22>> {
+  struct subtype_<arrow<T11, T12>, arrow<T21, T22>> {
     static constexpr bool value =
-      subtype<T21, T11>::value && subtype<T12, T22>::value;
+      subtype_<T21, T11>::value && subtype_<T12, T22>::value;
   };
   template <class T1, class T2>
-  static constexpr bool subtype_v = subtype<T1, T2>::value;
+  static constexpr bool subtype_v = subtype_<T1, T2>::value;
 
   // ------------------------------------------
   // Constr
@@ -201,49 +201,49 @@ namespace TORI_NS::detail {
   };
 
   template <class TyArrow, class Constr>
-  struct subst_constr {};
+  struct subst_constr_ {};
   template <class TyT1, class TyT2, class T1, class T2>
-  struct subst_constr<tyarrow<TyT1, TyT2>, constr<T1, T2>> {
+  struct subst_constr_<tyarrow<TyT1, TyT2>, constr<T1, T2>> {
     using type = constr<
       subst_t<tyarrow<TyT1, TyT2>, T1>,
       subst_t<tyarrow<TyT1, TyT2>, T2>>;
   };
   /// Apply TyArrow to Constr
   template <class TyArrow, class Constr>
-  using subst_constr_t = typename subst_constr<TyArrow, Constr>::type;
+  using subst_constr_t = typename subst_constr_<TyArrow, Constr>::type;
 
   template <class TyArrow, class Constrs>
-  struct subst_constr_all {
+  struct subst_constr_all_ {
     static_assert(false_v<TyArrow>, "Invalid argument(s)");
   };
   template <class TyT1, class TyT2, class... Cs>
-  struct subst_constr_all<tyarrow<TyT1, TyT2>, std::tuple<Cs...>> {
+  struct subst_constr_all_<tyarrow<TyT1, TyT2>, std::tuple<Cs...>> {
     using type = std::tuple<subst_constr_t<tyarrow<TyT1, TyT2>, Cs>...>;
   };
   /// Apply TyArrow to all constrains Cs
   template <class TyArrow, class Cs>
-  using subst_constr_all_t = typename subst_constr_all<TyArrow, Cs>::type;
+  using subst_constr_all_t = typename subst_constr_all_<TyArrow, Cs>::type;
 
   // ------------------------------------------
   // Occurs
   // ------------------------------------------
   template <class X, class T>
-  struct occurs {};
+  struct occurs_ {};
   template <class X, class T1, class T2>
-  struct occurs<X, arrow<T1, T2>> {
-    static constexpr bool value = occurs<X, T1>::value || occurs<X, T2>::value;
+  struct occurs_<X, arrow<T1, T2>> {
+    static constexpr bool value = occurs_<X, T1>::value || occurs_<X, T2>::value;
   };
   template <class X, class Tag>
-  struct occurs<X, value<Tag>> {
+  struct occurs_<X, value<Tag>> {
     static constexpr bool value = false;
   };
   template <class X, class Tag>
-  struct occurs<X, var<Tag>> {
+  struct occurs_<X, var<Tag>> {
     static constexpr bool value = std::is_same_v<X, var<Tag>>;
   };
   /// Does X occur in T?
   template <class X, class T>
-  static constexpr bool occurs_v = occurs<X, T>::value;
+  static constexpr bool occurs_v = occurs_<X, T>::value;
 
   // ------------------------------------------
   // Unify
@@ -251,7 +251,7 @@ namespace TORI_NS::detail {
 
   // error
   template <class ConstrList>
-  struct unify {
+  struct unify_ {
     using type = typename ConstrList::_error_here;
     static_assert(
       false_v<ConstrList>, "Unification error: Unsolvable constraints");
@@ -273,54 +273,54 @@ namespace TORI_NS::detail {
   template <class Var, class T, class Tail>
   struct unify_h<Var, T, Tail, true> {
     using type = append_tuple_t<
-      typename unify<subst_constr_all_t<tyarrow<Var, T>, Tail>>::type,
+      typename unify_<subst_constr_all_t<tyarrow<Var, T>, Tail>>::type,
       tyarrow<Var, T>>;
   };
   // helper2
   template <class Tag1, class Tag2, class Tail>
   struct unify_h2<Tag1, Tag2, Tail, true> {
-    using type = typename unify<Tail>::type;
+    using type = typename unify_<Tail>::type;
   };
 
   // empty set
   template <>
-  struct unify<std::tuple<>> {
+  struct unify_<std::tuple<>> {
     using type = std::tuple<>;
   };
   template <class Tag1, class Tag2, class... Cs>
-  struct unify<std::tuple<constr<value<Tag1>, value<Tag2>>, Cs...>> {
+  struct unify_<std::tuple<constr<value<Tag1>, value<Tag2>>, Cs...>> {
     using type = typename unify_h2<Tag1, Tag2, std::tuple<Cs...>>::type;
   };
   template <class Tag1, class Tag2, class... Cs>
-  struct unify<std::tuple<constr<var<Tag1>, var<Tag2>>, Cs...>> {
+  struct unify_<std::tuple<constr<var<Tag1>, var<Tag2>>, Cs...>> {
     using type = std::conditional_t<
       std::is_same_v<var<Tag1>, var<Tag2>>,
-      typename unify<std::tuple<Cs...>>::type,
+      typename unify_<std::tuple<Cs...>>::type,
       typename unify_h<var<Tag1>, var<Tag2>, std::tuple<Cs...>>::type>;
   };
   template <class T1, class Tag, class... Cs>
-  struct unify<std::tuple<constr<T1, var<Tag>>, Cs...>> {
+  struct unify_<std::tuple<constr<T1, var<Tag>>, Cs...>> {
     using type = std::conditional_t<
       std::is_same_v<T1, var<Tag>>,
-      typename unify<std::tuple<Cs...>>::type,
+      typename unify_<std::tuple<Cs...>>::type,
       typename unify_h<var<Tag>, T1, std::tuple<Cs...>>::type>;
   };
   template <class Tag, class T2, class... Cs>
-  struct unify<std::tuple<constr<var<Tag>, T2>, Cs...>> {
+  struct unify_<std::tuple<constr<var<Tag>, T2>, Cs...>> {
     using type = std::conditional_t<
       std::is_same_v<var<Tag>, T2>,
-      typename unify<std::tuple<Cs...>>::type,
+      typename unify_<std::tuple<Cs...>>::type,
       typename unify_h<var<Tag>, T2, std::tuple<Cs...>>::type>;
   };
   template <class S1, class S2, class T1, class T2, class... Cs>
-  struct unify<std::tuple<constr<arrow<S1, S2>, arrow<T1, T2>>, Cs...>> {
+  struct unify_<std::tuple<constr<arrow<S1, S2>, arrow<T1, T2>>, Cs...>> {
     using type =
-      typename unify<std::tuple<constr<S1, T1>, constr<S2, T2>, Cs...>>::type;
+      typename unify_<std::tuple<constr<S1, T1>, constr<S2, T2>, Cs...>>::type;
   };
 
   /// Unification
   template <class Cs>
-  using unify_t = typename unify<Cs>::type;
+  using unify_t = typename unify_<Cs>::type;
 
   // ------------------------------------------
   // Typing
@@ -348,35 +348,35 @@ namespace TORI_NS::detail {
   using nextgen_t = typename genvar<Gen>::next;
 
   template <class T, class Gen>
-  struct recon {};
+  struct recon_ {};
   template <class Tag, class Gen>
-  struct recon<TmValue<Tag>, Gen> {
+  struct recon_<TmValue<Tag>, Gen> {
     using type = typename TmValue<Tag>::type;
     using c = std::tuple<>;
     using gen = Gen;
   };
   template <class Tag, class Gen>
-  struct recon<TmVar<Tag>, Gen> {
+  struct recon_<TmVar<Tag>, Gen> {
     using type = typename TmVar<Tag>::type;
     using c = std::tuple<>;
     using gen = Gen;
   };
   template <class T, class Gen>
-  struct recon<TmClosure<T>, Gen> {
+  struct recon_<TmClosure<T>, Gen> {
     // unwrap
-    using type = typename recon<T, Gen>::type;
-    using gen = typename recon<T, Gen>::gen;
-    using c = typename recon<T, Gen>::c;
+    using type = typename recon_<T, Gen>::type;
+    using gen = typename recon_<T, Gen>::gen;
+    using c = typename recon_<T, Gen>::c;
   };
   template <class T, class... Ts, class Gen>
-  struct recon<TmClosure<T, Ts...>, Gen> {
+  struct recon_<TmClosure<T, Ts...>, Gen> {
     // t1
-    using _t1 = recon<T, Gen>;
+    using _t1 = recon_<T, Gen>;
     using _t1_t = typename _t1::type;
     using _t1_gen = typename _t1::gen;
     using _t1_c = typename _t1::c;
     // t2
-    using _t2 = recon<TmClosure<Ts...>, _t1_gen>;
+    using _t2 = recon_<TmClosure<Ts...>, _t1_gen>;
     using _t2_t = typename _t2::type;
     using _t2_gen = typename _t2::gen;
     using _t2_c = typename _t2::c;
@@ -386,9 +386,9 @@ namespace TORI_NS::detail {
     using c = _t2_c;
   };
   template <class T1, class T2, class Gen>
-  struct recon<TmApply<T1, T2>, Gen> {
-    using _t1 = recon<T1, Gen>;
-    using _t2 = recon<T2, typename _t1::gen>;
+  struct recon_<TmApply<T1, T2>, Gen> {
+    using _t1 = recon_<T1, Gen>;
+    using _t2 = recon_<T2, typename _t1::gen>;
     using _gen = typename _t2::gen;
     using type = genvar_t<_gen>;
     using gen = nextgen_t<_gen>;
@@ -397,9 +397,9 @@ namespace TORI_NS::detail {
       constr<typename _t1::type, arrow<typename _t2::type, type>>>;
   };
   template <class T, class Gen>
-  struct recon<TmFix<T>, Gen> {
+  struct recon_<TmFix<T>, Gen> {
     // recon T 
-    using _t = recon<T, Gen>;
+    using _t = recon_<T, Gen>;
     using _t_t = typename _t::type;
     using _t_gen = typename _t::gen;
     using _t_c = typename _t::c;
@@ -411,10 +411,10 @@ namespace TORI_NS::detail {
 
   /// Get type of term
   template <class Term>
-  using recon_type_t = typename recon<Term, taggen<0>>::type;
+  using recon_type_t = typename recon_<Term, taggen<0>>::type;
   /// Get constraint set of term
   template <class Term>
-  using recon_constr_t = typename recon<Term, taggen<0>>::c;
+  using recon_constr_t = typename recon_<Term, taggen<0>>::c;
 
   /// Infer type of term
   template <class Term>
