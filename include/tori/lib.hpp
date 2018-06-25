1ce6001af9c2e250585e9590a3340f876380d71d
mocabe <hisa.mocabe@gmail.com>
2018-06-25 21:50:39 +0900

    :construction: added Identity

---
+++ include/tori/lib.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 mocabe(https://github.com/mocabe)
+// This code is licensed under MIT license.
+
+#pragma once
+
+#include "core.hpp"
+
+#include "lib/Identity.hpp"
---
+++ include/tori/lib/Identity.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2018 mocabe(https://github.com/mocabe)
+// This code is licensed under MIT license.
+
+#pragma once
+
+#include "../core.hpp"
+
+namespace TORI_NS::detail {
+  namespace interface {
+    struct Identity : Function<Identity, Auto<class X>, Auto<class X>> {
+      ReturnType code() {
+        return arg<0>();
+      }
+    };
+  } // namespace interface
+} // namespace TORI_NS::detail
