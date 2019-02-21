53a03fab6c18fe5a92366b15521552c8bacef2b2
mocabe <hisa.mocabe@gmail.com>
2019-02-22 01:22:44 +0900

    :hammer: improved codegen on MSVC

--- include/tori/core/object_ptr_storage.hpp
+++ include/tori/core/object_ptr_storage.hpp
@@ -14,7 +14,7 @@ namespace TORI_NS::detail {
   struct object_ptr_storage
   {
     /// pointer tag values. stored in lowest 3 bits of 64bit data.
-    enum class pointer_tags : uint32_t
+    enum class pointer_tags : uint64_t
     {
       // clang-format off
 
@@ -23,12 +23,12 @@ namespace TORI_NS::detail {
       //                        tag (3bit)
       //                        ^^^^^^^^^^
 
-      pointer   = 0x00000000, //< plain pointer
-      apply     = 0x00000001, //< pointer to apply (optional)
-      exception = 0x00000002, //< pointer to exception (optional)
+      pointer   = 0x0, //< plain pointer
+      apply     = 0x1, //< pointer to apply (optional)
+      exception = 0x2, //< pointer to exception (optional)
 
-      extract_mask = 0x00000007, // ...0111
-      clear_mask   = 0xFFFFFFF8, // ...1000
+      extract_mask = 0x0000000000000007, // 0...0111
+      clear_mask   = 0xFFFFFFFFFFFFFFF8, // 1...1000
 
       // clang-format on
     };
@@ -36,20 +36,20 @@ namespace TORI_NS::detail {
     /// extract pointer tag flag
     pointer_tags get_pointer_tag() const noexcept
     {
-      uint32_t flag;
-      std::memcpy(&flag, &m_np.flag, sizeof(flag));
+      uint64_t tag;
+      std::memcpy(&tag, &m_tag, sizeof(tag));
       return static_cast<pointer_tags>(
-        flag & (uint32_t)pointer_tags::extract_mask);
+        tag & static_cast<uint64_t>(pointer_tags::extract_mask));
     }
 
     /// set new pointer tag flag
-    void set_pointer_tag(pointer_tags flag) noexcept
+    void set_pointer_tag(pointer_tags tag) noexcept
     {
-      uint32_t flg;
-      std::memcpy(&flg, &m_np.flag, sizeof(flg));
-      flg &= (uint32_t)pointer_tags::clear_mask;
-      flg |= (uint32_t)flag;
-      std::memcpy(&m_np.flag, &flg, sizeof(m_np.flag));
+      uint64_t tmp;
+      std::memcpy(&tmp, &m_tag, sizeof(tmp));
+      tmp &= static_cast<uint64_t>(pointer_tags::clear_mask);
+      tmp |= static_cast<uint64_t>(tag);
+      std::memcpy(&m_tag, &tmp, sizeof(m_tag));
     }
 
     /// get pointer
@@ -57,13 +57,13 @@ namespace TORI_NS::detail {
     Object* ptr() const noexcept
     {
       // load as non-pointer
-      non_pointer np;
-      std::memcpy(&np, &m_np, sizeof(np));
+      uint64_t tag;
+      std::memcpy(&tag, &m_tag, sizeof(tag));
       // clear pointer tag
-      np.flag &= (uint32_t)pointer_tags::clear_mask;
+      tag &= static_cast<uint64_t>(pointer_tags::clear_mask);
       // cast to pointer
       Object* ptr;
-      std::memcpy(&ptr, &np, sizeof(ptr));
+      std::memcpy(&ptr, &tag, sizeof(ptr));
       return ptr;
     }
 
@@ -86,53 +86,24 @@ namespace TORI_NS::detail {
       return ptr()->refcount.load() == 0;
     }
 
-    /// non pointer struct
-    struct non_pointer
-    {
-      /// pointer tag and flags
-      uint32_t flag;
-      /// reserved for future use
-      uint32_t reserved;
-    };
-
-  private:
-    // -------------------------------------------
-    // Helper functions for constexpr initializer.
-    // gcc and clang accepst non-constexpr call but msvc doesn't.
-    // Can be removed in C++20 since memcpy will become constexpr.
-
-    constexpr uint32_t get_pointer_tag_constexpr() const noexcept
-    {
-      return m_np.flag & (uint32_t)pointer_tags::extract_mask;
-    }
-
-    constexpr void set_pointer_tag_constexpr(pointer_tags flag) noexcept
-    {
-      m_np.flag &= (uint32_t)pointer_tags::clear_mask;
-      m_np.flag |= (uint32_t)flag;
-    }
-
   public:
 
     /// Ctor
     constexpr object_ptr_storage(Object* p) noexcept
       : m_ptr {p}
     {
-      set_pointer_tag_constexpr(pointer_tags::pointer);
     }
 
     /// Ctor
     constexpr object_ptr_storage(const Object* p) noexcept
       : m_ptr {p}
     {
-      set_pointer_tag_constexpr(pointer_tags::pointer);
     }
 
     /// Ctor
     constexpr object_ptr_storage(nullptr_t p) noexcept
       : m_ptr {p}
     {
-      set_pointer_tag_constexpr(pointer_tags::pointer);
     }
 
   private:
@@ -140,8 +111,8 @@ namespace TORI_NS::detail {
     {
       /// pointer to object
       const Object* m_ptr;
-      /// non pointer value
-      non_pointer m_np;
+      /// pointer tag
+      uint64_t m_tag;
     };
   };
 
