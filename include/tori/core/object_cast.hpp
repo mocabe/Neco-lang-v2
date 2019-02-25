e751b264ebbc605e73eab2f54df8da98fd3ca6b3
mocabe <hisa.mocabe@gmail.com>
2019-02-25 12:31:53 +0900

    :hammer: fix potential undefined behaviour

--- include/tori/core/object_cast.hpp
+++ include/tori/core/object_cast.hpp
@@ -15,14 +15,28 @@ namespace TORI_NS::detail {
   {
     if (likely(!obj.is_static()))
       obj.head()->refcount.fetch_add();
-    return static_cast<T*>(obj.get());
+
+    if constexpr (std::is_base_of_v<T, U>)
+      return static_cast<T*>(obj.get());
+    else if constexpr (std::is_base_of_v<U, T>)
+      // downcast to proxy types (empty derived class from Object) using
+      // static_cast IS undefined behaviour. reinterpret_cast is not UB when T
+      // and U are layout compatible.
+      return reinterpret_cast<T*>(obj.get());
+    else
+      static_assert(false_v<T, U>);
   }
 
   /// static object cast
   template <class T, class U>
   [[nodiscard]] object_ptr<T> static_object_cast(object_ptr<U>&& obj)
   {
-    return static_cast<T*>(obj.release());
+    if constexpr (std::is_base_of_v<T, U>)
+      return static_cast<T*>(obj.release());
+    else if constexpr (std::is_base_of_v<U, T>)
+      return reinterpret_cast<T*>(obj.release());
+    else
+      static_assert(false_v<T, U>);
   }
 
 } // namespace TORI_NS::detail
