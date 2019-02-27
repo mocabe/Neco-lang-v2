891a981d1120d2010ba380736111ea0ca93d6223
mocabe <hisa.mocabe@gmail.com>
2019-02-27 13:59:19 +0900

    :hammer: refactoring

--- include/tori/core/object_cast.hpp
+++ include/tori/core/object_cast.hpp
@@ -16,27 +16,27 @@ namespace TORI_NS::detail {
     if (likely(!obj.is_static()))
       obj.head()->refcount.fetch_add();
 
-    if constexpr (std::is_base_of_v<T, U>)
-      return static_cast<T*>(obj.get());
-    else if constexpr (std::is_base_of_v<U, T>)
+    if constexpr (std::is_base_of_v<U, T> && sizeof(T) == sizeof(U))
       // downcast to proxy types (empty derived class from Object) using
-      // static_cast IS undefined behaviour. reinterpret_cast is not UB when T
-      // and U are layout compatible.
+      // static_cast IS undefined behaviour. But it seems that using
+      // reinterpret_cast is not UB when T and U are layout compatible. Since
+      // empty derived classes from standard layout class (i.e. Object) are also
+      // standard lyaout (and these are layout compatible since no additional
+      // members are introduced), we can use reinterpret_cast here.
       return reinterpret_cast<T*>(obj.get());
     else
-      static_assert(false_v<T, U>);
+      // it's undefined behavior if `obj` is not an actual object of T.
+      return static_cast<T*>(obj.get());
   }
 
   /// static object cast
   template <class T, class U>
   [[nodiscard]] object_ptr<T> static_object_cast(object_ptr<U>&& obj)
   {
-    if constexpr (std::is_base_of_v<T, U>)
-      return static_cast<T*>(obj.release());
-    else if constexpr (std::is_base_of_v<U, T>)
+    if constexpr (std::is_base_of_v<U, T> && sizeof(T) == sizeof(U))
       return reinterpret_cast<T*>(obj.release());
     else
-      static_assert(false_v<T, U>);
+      return static_cast<T*>(obj.release());
   }
 
 } // namespace TORI_NS::detail
